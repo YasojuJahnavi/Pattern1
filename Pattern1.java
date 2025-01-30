b734a006685dec2b633d1738e76a0f2fcf7ec72e
YasojuJahnavi <yasojujahnavi.jh7@gmail.com>
2025-01-30 19:26:36 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,11 @@
+public class Pattern1 {
+    public static void main(String[] args){
+        int n=5;
+        for (int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){//j(colums) value is dependent on the i(rows) 
+                System.out.print("*");//use print statement for execution , whenre printLn prints the statementd in new line.
+            }
+            System.out.print("\n");
+}
+}
+}
